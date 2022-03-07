04931214d12f775f3d53d2fa080c2dfdbbf20312
SkLs1337 <andygrecu2000@gmail.com>
2022-03-08 00:01:15 +0200

    fix previous commit

--- routes/orders.js
+++ routes/orders.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const {
-    getOrders,
-    getOrder,
     createOrder,
-    updateOrder,
-    deleteOrder,
 } = require('../controllers/orders');
 
 const { protect, authorize } = require('../middleware/auth');
