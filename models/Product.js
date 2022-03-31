dfcbd081388b639a0ea53a3f6fa42547f240954d
Andy Grecu <andygrecu2000@gmail.com>
2022-03-31 18:40:55 +0300

    update product

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const stripe = require("stripe")(process.env.STRIPE_API_KEY);
 
 const ProductSchema = new mongoose.Schema({
   title: {
@@ -26,10 +27,27 @@ const ProductSchema = new mongoose.Schema({
     required: [true, "The quantity must be a positive number"],
     min: 0,
   },
+  productStripe: {
+    type: Object,
+  },
+  slug: {
+    type: String,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
   },
+});
+
+ProductSchema.pre("save", async function (next) {
+  const product = await stripe.products.create({ name: this.title });
+  const unit_amount = this.price * 100;
+  this.productStripe = await stripe.prices.create({
+    product: product.id,
+    unit_amount: unit_amount,
+    currency: "RON",
+  });
+  next();
 });
 
 module.exports = mongoose.model("Product", ProductSchema);
